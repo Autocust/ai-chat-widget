6138fd74522520beeb85ac197a47120f12b715e9
Enrico Deleo <hello@enricodeleo.com>
2025-10-17 11:28:31 +0200

    refactor: spacing

--- components/Message.svelte
+++ components/Message.svelte
@@ -48,14 +48,15 @@
     max-width: 100%; /* Allow full width for messages with CTA buttons */
   }
 
-  
   .user-message {
     margin-left: auto;
   }
+
   .assistant-message,
   .human_agent-message {
     margin-right: auto;
   }
+
   .system-message {
     margin-left: auto;
     margin-right: auto;
@@ -63,41 +64,49 @@
     font-style: italic;
     color: #888;
   }
+
   .message {
     display: flex;
     align-items: flex-start;
     gap: 8px;
   }
+
   .message-icon {
     width: 32px;
     height: 32px;
     flex-shrink: 0;
   }
+
   .message-content {
     flex-grow: 1;
     word-wrap: break-word;
     padding: 5px 10px;
     border-radius: 5px;
   }
+
   .user-message .message-content {
     background-color: var(--user-msg-bg);
     color: var(--user-msg-text);
   }
+
   .assistant-message .message-content {
     background-color: var(--assistant-msg-bg);
     color: var(--assistant-msg-text);
   }
+
   .human_agent-message .message-content {
     background-color: var(--human-agent-msg-bg);
     color: var(--human-agent-msg-text);
   }
+
   .message-content :global(a) {
     color: var(--link-color);
   }
+
   :global(.message-content ul), :global(.message-content ol) {
     padding-inline-start: 20px;
   }
-  
+
   .message-links {
     display: flex;
     flex-direction: column;
